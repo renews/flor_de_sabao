1dbbaec51202e7c26adfa2f90bbe7723e819a47e
Prelang Builder <builder@prelang.com>
2014-04-10 12:59:40 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlorDeSabao::Application.config.secret_key_base = '8217a74fc206c0edce7ebf081ee38c792f3dcbe94164a22422dd2085dfc45f1cf4feea02cd9b295bf2f61389eb4da7a7b2ff8f329673c1efacfe582c6784ef7a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FlorDeSabao::Application.config.session_store :cookie_store, key: '_flor_de_sabao_session'
