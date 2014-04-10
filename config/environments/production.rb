5b840201618192c86e6a7eaefd23b37a8b7f1de6
Prelang Builder <builder@prelang.com>
2014-04-10 13:00:26 +0000

    Injected Google Analytics tracker ID configuration into production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,9 @@ FlorDeSabao::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #->Prelang
+  GA.tracker = ""
+
 end
+
